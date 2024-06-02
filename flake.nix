2c803d9ae80d930ffb7d14576c4bde147b116e32
ElrohirGT <elrohirgt@gmail.com>
2024-06-02 13:21:59 -0600

    feat: Added full vim config

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "The lord of the Code's Vim configuration for ICPC Challenge";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixVim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixVim,
+  }: let
+    forAllSystems = {
+      pkgs ? nixpkgs,
+      function,
+    }:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-macos"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ] (system:
+        function {
+          inherit system;
+          pkgs = import pkgs {
+            config.allowUnfree = true;
+            # overlays = [];
+          };
+        });
+    buildVimModule = {
+      system,
+      module,
+    }: let
+      nixvimPkgs = nixVim.legacyPackages.${system};
+      nixVimModule = {
+        module = import "${module}";
+      };
+    in
+      nixvimPkgs.makeNixvimWithModule nixVimModule;
+  in {
+    devShells = forAllSystems {
+      function = {
+        pkgs,
+        system,
+      }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk20
+          ];
+        };
+      };
+    };
+
+    packages = forAllSystems {
+      function = {
+        pkgs,
+        system,
+      }: {
+        default = buildVimModule {
+          inherit system;
+          module = ./full.nix;
+        };
+      };
+    };
+  };
+}
---
+++ full.nix
@@ -0,0 +1,106 @@
+{pkgs, ...}: {
+  opts = {
+    number = true;
+    relativenumber = true;
+
+    shiftwidth = 2;
+    tabstop = 2;
+    softtabstop = 2;
+
+    expandtab = false;
+    hlsearch = false;
+    incsearch = true;
+  };
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+
+  plugins = {
+    lspkind.enable = true;
+    oil.enable = true;
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>ff" = {
+          action = "find_files";
+          options = {
+            desc = "[F]ind [f]iles in CWD";
+          };
+        };
+        "<leader>fg" = {
+          action = "live_grep";
+          options = {
+            desc = "[F]ind file according to content that matches [G]rep";
+          };
+        };
+        "<leader>fr" = {
+          action = "lsp_references";
+          options = {
+            desc = "[F]ind [R]eferences";
+          };
+        };
+        "<leader>fi" = {
+          action = "lsp_implementations";
+          options = {
+            desc = "[F]ind [I]mplementations";
+          };
+        };
+        "<leader>fs" = {
+          action = "lsp_workspace_symbols";
+          options = {
+            desc = "[F]ind workspace [S]ymbols";
+          };
+        };
+      };
+    };
+
+    treesitter = {
+      enable = true;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        java
+      ];
+    };
+
+    lsp = {
+      enable = true;
+      servers = {
+        java-language-server.enable = true;
+      };
+    };
+
+    luasnip.enable = true;
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+
+          # Manually trigger a completion from nvim-cmp
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-y>" = "cmp.mapping.confirm({ select = true })";
+        };
+      };
+    };
+  };
+
+  # Setting up the completion engine to use luasnip
+  plugins.cmp.settings.snippet.expand = ''function(args) require('luasnip').lsp_expand(args.body) end'';
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = ":Oil<CR>";
+      options = {
+        desc = "Toggle Oil";
+      };
+    }
+  ];
+}
